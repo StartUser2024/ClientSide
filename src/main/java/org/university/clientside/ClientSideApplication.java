8b42049456dd7384dfba25723d9149b1a7f3d6f6
User <kveselov888@gmail.com>
2024-07-09 14:50:42 +0300

    first commit Initial commit

--- src/main/java/org/university/clientside/ClientSideApplication.java
+++ src/main/java/org/university/clientside/ClientSideApplication.java
@@ -1,13 +1,6 @@
 package org.university.clientside;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class ClientSideApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ClientSideApplication.class, args);
-    }
 
-}
