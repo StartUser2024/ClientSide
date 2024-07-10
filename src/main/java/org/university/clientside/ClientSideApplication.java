9cbf7beb9e2254a54355734124560bc2ee6ba9e1
User <kveselov888@gmail.com>
2024-07-10 13:23:48 +0300

    10.07.2024 every types

--- src/main/java/org/university/clientside/ClientSideApplication.java
+++ src/main/java/org/university/clientside/ClientSideApplication.java
@@ -1,5 +1,7 @@
 package org.university.clientside;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import lombok.SneakyThrows;
 import org.json.JSONObject;
 import java.net.*;
@@ -11,33 +13,50 @@ public class ClientSideApplication {
     @SneakyThrows
     public static void main(String args[]) {
         try {
-            File file = new File("C:\\Users\\User\\Downloads\\test.txt");
+            System.out.println("Connection accepted by the Server..");
+            SocketChannel client = SocketChannel.open(
+                    new InetSocketAddress("localhost", 808));
+
+            File file = new File("C:\\Users\\User\\Downloads\\QWERTY.pptx");
             FileInputStream fis = new FileInputStream(file);
 
             byte[] payload = new byte[(int) file.length()];
             fis.read(payload);
             fis.close();
 
-            JSONObject json = new JSONObject();
-            json.put("fileName", "TEST.txt");
-            json.put("length", file.length());
-            json.put("payload", new String(payload));
-            System.out.println(payload);
-            String jsonString =  json.toString();
-            System.out.println(
-                    "Connection accepted by the Server..");
-            SocketChannel client = SocketChannel.open(
-                    new InetSocketAddress("localhost", 808));
+            Gson gson = new GsonBuilder().create();
+            String json = gson.toJson(new FileData(file.getName(), file.length(), payload));
+            System.out.println(json);
+            ByteBuffer buf = ByteBuffer.wrap(json.getBytes());
 
-            ByteBuffer buffer = ByteBuffer.allocate(1000);
-            buffer.put(jsonString.getBytes());
-            buffer.flip();
-            int bytesWritten = client.write(buffer);
-            System.out.println(String.format("Sending JSON: %s\nbufforBytes: %d", jsonString, bytesWritten));
+//            JSONObject json = new JSONObject();
+//            json.put("fileName", "TEST.txt");
+//            json.put("length", file.length());
+//            json.put("payload", new String(payload));
+//            System.out.println(payload);
+//            String jsonString =  json.toString();
+
+
+//            ByteBuffer buffer = ByteBuffer.allocate(1000);
+//            buffer.put(jsonString.getBytes());
+//            buffer.flip();
+            client.write(buf);
+            //System.out.println(String.format("Sending JSON: %s\nbufforBytes: %d", jsonString, bytesWritten));
             client.close();
             System.out.println("Client connection closed");
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+    private static class FileData {
+        private final String name;
+        private final long length;
+        private final byte[] data;
+
+        public FileData(String name, long length, byte[] data) {
+            this.name = name;
+            this.length = length;
+            this.data = data;
         }
     }
 }
