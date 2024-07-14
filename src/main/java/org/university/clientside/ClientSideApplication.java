270c5c8f389807431ccc1973e5f6c09a3925468b
User <kveselov888@gmail.com>
2024-07-14 10:18:06 +0300

    14.07.2024 Dependencies OK, Reading OK, Multithreading ERROR

--- src/main/java/org/university/clientside/ClientSideApplication.java
+++ src/main/java/org/university/clientside/ClientSideApplication.java
@@ -8,20 +8,22 @@ import java.nio.channels.SocketChannel;
 public class ClientSideApplication {
     public static void main(String args[]) {
         try {
+            File file = new File("C:\\Users\\User\\Downloads\\QWERTY.pptx");//qtcreator-windows-x64-mingw-13.0.0.7z");
+            FileInputStream fis = new FileInputStream(file);
             System.out.println("Connection accepted by the Server..");
             SocketChannel client = SocketChannel.open(
                     new InetSocketAddress("localhost", 808));
 
-            File file = new File("C:\\Users\\User\\Downloads\\qtcreator-windows-x64-mingw-13.0.0.7z");
-            FileInputStream fis = new FileInputStream(file);
+            ByteBuffer buffer = ByteBuffer.allocate(1024*8);
             String message = file.getName() + "\r\n" + file.length() + "\r\n";
-            ByteBuffer buffer = ByteBuffer.wrap(message.getBytes());
+            int count;
+            buffer.put(message.getBytes());
+            System.out.println(new String(buffer.array(), 0, message.getBytes().length));
+            buffer.flip();
             client.write(buffer);
             buffer.clear();
 
-            int count;
             while ((count = fis.read(buffer.array())) != -1) {
-                System.out.println(new String(buffer.array(), 0, count));
                 buffer.put(buffer.array(), 0, count);
                 buffer.flip();
                 client.write(buffer);
